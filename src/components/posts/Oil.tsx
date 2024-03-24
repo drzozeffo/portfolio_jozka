cddaafa5bca0bea189a52638fb4dd40e51901324
drzozeffo <j.szym7334@gmail.com>
2024-03-24 23:26:59 +0100

    Opis oleju
    Aktualizacja opisów. Dodanie zdjęcia dania, orki korzeni, wieńca dożynkowego, filmu z koszenia kombajnem i zdjęcia z koszenia kombajnem i zdjęcia skoszonego lnu.

--- src/components/posts/Oil.tsx
+++ src/components/posts/Oil.tsx
@@ -11,6 +11,12 @@ import oil_len_kwitnacy from "../../images/oil_len_kwitnacy.jpg";
 import oil_na_ciagniku from "../../images/oil_na_ciagniku.jpg";
 import oil_pszczola from "../../images/oil_pszczoła.jpg";
 import oil_len_dojrzaly from "../../images/oil_len_dojrzaly.jpg";
+import oil_bociany from "../../images/oil_bociany.jpg";
+import oil_przekaska from "../../images/oil_przekaska.jpg";
+import oil_orka_korzenie from "../../images/oil_orka_korzenie.jpg";
+import oil_wieniec from "../../images/oil_wieniec.jpg";
+import oil_koszenie from "../../images/oil_koszenie.jpg";
+import oil_nasiona from "../../images/oil_nasiona.jpg";
 
 const Ad = styled.div`
   width: inherit;
@@ -18,6 +24,24 @@ const Ad = styled.div`
   border-radius: 30px;
   box-shadow: 10px 10px 10px rgba(0, 0, 0, 0.15);
   margin-bottom: 30px;
+`;
+
+const Ab = styled.div`
+  width: inherit;
+  column-gap: 40px;
+  text-align: center;
+`;
+
+const Xd = styled.div`
+  text-align: center;
+`;
+
+const Image_ab = styled.div`
+  float: left;
+  text-align: center;
+  border-top: 5px solid #f5f5f5;
+  border-left: 5px solid #f5f5f5;
+  border-right: 5px solid #f5f5f5;
 `;
 
 const Image_ad = styled.div`
@@ -59,6 +83,8 @@ const StyledA = styled.a`
 `;
 
 
+
+
 export const Oil = () => (
 <Wrapper>
     <Title>Olej lniany</Title>
@@ -67,111 +93,152 @@ export const Oil = () => (
     <Ad>
       <Image_ad><Img src={oil} width="480px" alt="Olej lniany" /></Image_ad>
       <Column>
-      <p><strong>Odkryj Tajemnicę Zdrowego Życia z Olejem Lnianym Zimnotłoczonym z Mikołajówki!</strong></p>
-      <p>Czy marzysz o naturalnym sposobie na poprawę swojego zdrowia i samopoczucia? Olej lniany zimnotłoczony to odpowiedź na Twoje potrzeby! Pochodzący prosto z serca suwalskiej ziemi, z malowniczej Mikołajówki, olej lniany jest tłoczony na zimno z najwyższej jakości nasion lnu.</p>
+      <p><strong>Odkryj Tajemnicę Zdrowego Życia z Olejem Lnianym z Mikołajówki!</strong></p>
+      <p>Czy marzysz o naturalnym sposobie na dostarczenie swojemu organizmowi cennych składników? Olej lniany to odpowiedź na Twoje potrzeby! Pochodzący prosto z serca suwalskiej ziemi, z malowniczej Mikołajówki, olej lniany jest tłoczony na zimno z nasion lnu, z własnej ekologicznej uprawy.</p>
       </Column>
     </Ad>
 
-    <Title>Poznaj historię...</Title>
-  
-    <p><b>Pierwszy świadomy kontakt z olejem lnianym</b></p>
-    <p>Podczas studiów w Białymstoku mieszkałem w domu studenckim. Mój współlokator miał w zwyczaju dodawać do swoich posiłków olej lniany (na przykład, do kapusty kiszonej albo polewał nim ugotowane ziemniaki). Nie interesowałem się tym zbytnio, ale w końcu postanowiłem go zapytać. Spróbowałem tego oleju i okazało się, że obiad z dodatkiem oleju lnianego (polany dopiero po podaniu dania, nie w trakcie gotowania/smażenia!) był dużo smaczniejszy.</p>
-    {/* [Zdjęcie ugotowanych ziemniaków polanych olejem lnianym] */}
-      
-    <p><b>Konsultacje z wiedzą rodzinną</b></p>
-    <p>Podczas rodzinnych spotkań dowiedziałem się, że w Polsce len był kiedyś rośliną uprawianą przez każdego rolnika. W pewnym okresie jego uprawa była obowiązkowa, gdyż każdy musiał odpowiednią ilość oddać Państwu w ramach podatku. 
-    Postanowiłem sprawę omówić z babcią Gienią.</p>
-      
-    {/* [Zdjęcie babci] */}
-    <p>Babcia szczegółowo opisała mi czynności związane z uprawą lnu, aż po otrzymanie kompletnych ubrań. Było bardzo ciekawe. Ale zaraz! A co z olejem?
-Po wymłóceniu nasion cepami, jeden z mieszkańców wioski wiózł nasiona do tłoczni do Suwałk. Po wytłoczeniu olej był przechowywany w kanie w składówce. Do czego używano wtedy tego oleju? Okazuje się, że nie tylko do celów spożywczych. Wykorzystywaną cechą oleju lnianego były jego właściwości smarujące. Na przykład, smarowano nim osie w wozach, żeby zmniejszyć opór toczenia się osi w prowizorycznych łożyskach, zwiększyć ich żywotność i zlikwidować piski.</p>
-      
-    <p><b>Popularne danie z olejem lnianym</b></p>
-    <p>Zapytałem babcię o przykład dania, do którego przygotowania był używany olej lniany. Usłyszałem w odpowiedzi, że przygotowanie jej ulubionego dania wygląda następująco:</p>
-    <ol>
-      <li>Pokrój drobno jedno cebulę i wsyp do miski.</li>
-      <li>Dodaj trochę oleju lnianego i posól.</li>
-      <li>Ukrój pajdę chleba, maczaj ją w przygotowanej miskturze i ciesz się smakiem. 🙂</li>
-    </ol>
+    
 
-    {/* [Zdjęcie dania] */}
+    <Title>Jak to wszystko się zaczęło...</Title>
+  
+    <p><b>Pierwszy kontakt z olejem lnianym</b></p>
+    <p>Podczas studiów w mieszkałem w domu studenckim. Mój współlokator miał w zwyczaju dodawać do swoich posiłków olej lniany (przykładowo do kapusty kiszonej albo polewał nim ugotowane ziemniaki). Nie interesowałem się tym zbytnio, ale w końcu postanowiłem spróbować tego oleju. Posiłek z dodatkiem oleju lnianego okazał się przepyszny! Uwaga! Olej lniany został dodany do już gotowego dania. Nie nadaje się do gotowania, ani tym bardziej do smażenia!</p>
+
+
+   
+      
+    <p><b>Tło historyczne uprawy lnu – wiedza rodzinna</b></p>
+    <p>Podczas rodzinnych spotkań dowiedziałem się, że w Polsce len był kiedyś rośliną uprawianą przez każdego rolnika. W pewnym okresie jego uprawa była obowiązkowa, gdyż każdy gospodarz musiał określoną ilość tej rośliny oddać Państwu w ramach podatku. 
+Więcej o tej roślinie dowiedziałem się od babci Eugenii. Babcia szczegółowo opisała mi czynności związane z uprawą lnu, od przygotowania pola, aż po otrzymanie kompletnych ubrań. Z ciekawością wysłuchałem opisu całej tej procedury. Ale zaraz! A co z olejem? Nasiona lnu wymłócano ręcznie cepami na klepisku. Następnie jeden z mieszkańców wsi zaprzęgał wóz końmi, zbierał nasiona od sąsiednich gospodarzy i wiózł je do tłoczni do Suwałk. Po wytłoczeniu olej był przechowywany w kanie w składówce. Do czego używano wtedy tego oleju? Okazuje się, że nie tylko do celów spożywczych. Wykorzystywaną cechą oleju lnianego były jego właściwości smarujące. Jednym z takich zastosowań było smarowanie nim osi w wozach, żeby zmniejszyć ich opór toczenia się w prowizorycznych łożyskach.</p>
+      
+    <p><b>Popularna przekąska z olejem lnianym</b></p>
+    <p>Zapytałem babcię o przykład posiłku z dodatkiem oleju lnianego. Usłyszałem w odpowiedzi opis prostej przekąski. Pokrojoną drobno cebulę polewano olejem lnianym, posypywano szczyptą soli. Pajdę chleba maczano w przygotowanej miksturze i cieszono się smakiem 🙂</p>
+    
+
+    <Image>
+      <Img src={oil_przekaska} width="1080px" />
+      <p><i>Prosta i smaczna przekąska – olej lniany, cebulka, szczypta soli i chleb z własnego wypieku.</i></p>
+    </Image>
 
     <p><b>Własny olej lniany</b></p>
-    <p>Czy smak tego jakże skomplikowanego dania wzbudził we mnie pragnienie tłoczenia własnego oleju? Jeszcze nie.
-Moja druga babcia już długo chorowała na ciągle postępującą demencję. Próbowaliśmy wielu suplementów i zabiegów, lecz nic nie wstrzymywało tej bardzo przykrej choroby. Pamiętam, że wtedy wyczytałem gdzieś, że najbardziej nietrwałe mikroelementy w oleju lnianym rozpadają się już kilka godzin po wytłoczeniu oleju, więc wywnioskowałem z tego, że warto byłoby wytłoczyć taki olej we własnym zakresie, żeby mieć pewność, że jest świeży. .Wtedy postanowiłem rozpocząć działania w tym kierunku.
-Poszukiwania zacząłem od ekologicznych nasion lnu. Trafiłem na ogłoszenie. Sprzedawca bardzo sympatyczny, opowiedział mi o uprawie lnu od A do Z! Poradziłem się również odnośnie prasy do oleju. W efekcie kupiłem od niego nasiona lnu i zamówiłem prasę do tłoczenia oleju.</p>
+    <p>Czy smak tego jakże skomplikowanego dania wzbudził we mnie pragnienie tłoczenia własnego oleju lnianego? To jeszcze nie ten moment. Z racji na dużą zawartość kwasów omega zaczęliśmy podawać olej lniany dla naszej babci chorej na demencję. Dowiedziałem się też, że najbardziej nietrwałe mikroelementy w oleju lnianym rozpadają się w stosunkowo krótkim czasie po wytłoczeniu oleju, więc wywnioskowałem z tego, że warto byłoby wytłoczyć taki olej we własnym zakresie, żeby mieć pewność, że jest świeży. Wtedy postanowiłem rozpocząć działania w tym kierunku. Poszukiwania zacząłem od nasion lnu z ekologicznej uprawy. Znalazłem ogłoszenie. Sprzedawca okazał się bardzo sympatyczny i opowiedział mi o ekologicznej uprawie lnu od A do Z! Poradziłem się również odnośnie prasy do oleju. W końcu kupiłem od niego nasiona lnu i zamówiłem poleconą prasę do tłoczenia oleju.</p>
     
     
-    <p><b><i>Pierwsza próba uprawy lnu</i></b></p>
+    <p><b>Pierwsza próba uprawy lnu</b></p>
     {/* [zdjęcie pierwszej uprawy lnu] */}
 
-    <p>Pierwsza próba nie wyszła zbyt dobrze, gdyż len przegrał konkurencję z chwastami na polu. Również problematyczne było skoszenie go przez najęty kombajn. Nie był on przystosowany do ścinania tak włóknistej rośliny jaką jest len. Aczkolwiek ostatecznie udało się otrzymać ok. 15 kg nasion lnu...</p>
+    <p>Pierwsza próba uprawy tej rośliny miała miejsce w roku 2022 i nie wyszła zbyt dobrze, gdyż len przegrał konkurencję z chwastami na polu. Również problematyczne było skoszenie go przez najęty kombajn. Kombajn nie był przystosowany do ścinania tak włóknistej rośliny jaką jest len. Aczkolwiek ostatecznie udało się otrzymać kilkanaście kilogramów lnu.</p>
    
    
     <Title>Uprawa lnu 2023</Title>
+        
+    <p>Mając już jakieś doświadczenie, po raz kolejny podjąłem się uprawy lnu. Miejsce tegorocznej uprawy to ponad 20 letnia łąka, która przez cały okres swojego istnienia nie była nawożona nawozami sztucznymi ani traktowana środkami chemicznymi. Pierwszym zabiegiem była kultywacja, a następnie grunt został zaorany. W zwalczaniu insektów pomagały bociany. Zjadały pędraki czerwcowych chrabąszczy, drutowce i inne szkodniki.</p>
     
+
+    <Ab>
+      <Image_ab><Img src={oil_orka} width="480px" />
+      
+      </Image_ab>
+      
+      <Image_ab><Img src={oil_orka_korzenie} width="480px" />
+      
+      </Image_ab>
+    </Ab>
+
+
+    <Xd>
+      <p><i>Orka - góry prawie jak w świętokrzyskim! Wiele lat bez orki pozwoliło drzewom zapuścić korzenie kilkanaście metrów w głąb łąki!</i></p>
+      </Xd>
+    
+    
+
+    
+
+
+    <p>Następnie przeprowadzono kultywację oraz bronowanie. Do siewu przystąpiłem na początku maja. Po zrobieniu próby kręconej, ziarno zasiałem rozsiewaczem KOS-em. Następnie pole zostało zabronowane i zwałowane. Aż do czasu żniw nie były już wykonywane żadne zabiegi agrotechniczne.</p>
+
     <Image>
       <Img src={oil_na_ciagniku} width="1080px" />
-      <p><i>Ursus C-360-3P z gospodarzem na masce.</i></p>
-    </Image>
-    
-    <p>Mając już jakieś doświadczenie po raz kolejny podjąłem się uprawy lnu. Miejsce tegorocznej uprawy to ponad 20 letnia łąka, która przez cały okres swojego istnienia nie była nawożona nawozami sztucznymi ani traktowana środkami chemicznymi. Grunt na wiosnę został zaorany. Następnie przeprowadzono kultywację oraz bronowanie. W zwalczaniu insektów pomagały bociany (zjadały larwy chrabąszczy).</p>
-    
-    <Image>
-      <Img src={oil_orka} width="480px" />
-      <p><i>Orka - góry prawie jak w świętokrzyskim!</i></p>
+      <p><i>Ursus C-360-3P z gospodarzem na masce. Wałowanie pola - ostatni zabieg agrotechniczny.</i></p>
     </Image>
 
-    <Image>
-      <Img src={oil_walowanie} width="480px" />
-      <p><i>Wałowanie - ostatni zabieg agrotechniczny</i></p>
-    </Image>
-
-    
-    <p>Do siewu przystąpiłem na początku maja. Po zrobieniu próby kręconej, ziarno zasiałem rozsiewaczem KOS-em. Następnie pole zostało zabronowane i zwałowane. Aż do czasu żniw, nie były wykonywane żadne zabiegi agrotechniczne.</p>
     {/* Wegetacja rośliny */}
-    <p>W tym roku z racji na suszę len długo nie wschodził. Niepokoiło mnie to i już myślałem, że znowu nic z tego. Ale gdy po długim okresie suszy spadł deszcze, rośliny niemal z dnia na dzień wystrzeliły ku górze. Len w początkowej fazie wzrostu  przypomina wschodzącą rzodkiewkę :).</p>
+    <p>W tym roku z racji na suszę len długo nie wschodził. Niepokoiło mnie to i już zacząłem się martwić, że znowu nic z tego. Ale gdy po długim okresie suszy spadł deszcz, wszystkie rośliny niemal z dnia na dzień wystrzeliły ku górze. Len w początkowej fazie wzrostu wygląda jak wschodząca rzodkiewka :).
+    Len kwitnie bardzo charakterystycznie. Ma bardzo delikatne kwiaty. Z moich obserwacji wynika, że kwitnie od rana do południa. Po południu płatki kwiatów opadają. I tak codziennie przez około 2 tygodnie. Myślę, że pole kwitnącego lnu jest bardzo dobrym tłem do zdjęć ślubnych i innych okazji :).</p>
     {/* [Zdjęcia rosnącego lnu] */}
     
-    <p>Len kwitnie bardzo charakterystycznie. Ma bardzo delikatne kwiaty. Kwitnie od rana do południa, potem płatki kwiatów opadają. I tak codziennie przez około 2 tygodnie. Myślę, że pole kwitnącego lnu jest bardzo dobrym tłem do zdjęć ślubnych i innych okazji :).</p>
+    
     
     <Image>
       <Img src={oil_len_kwitnacy} width="1080px" />
-      <p><i>Mieniący się odcieniami fioletu kwitnący len otoczony zielonym tłem łodyg pod błękitnym niebem</i></p>
+      <p><i>Mieniący się odcieniami fioletu kwitnący len otoczony zielonym tłem łodyg pod chmurami wędrującymi po błękitnym niebie.</i></p>
     </Image>
 
-    <p>Pewnego razu podczas oglądania kwitnącego lnu zauważyłem mnóstwo pszczół. Lądowały na kwiatach i zbierały pyłek. Nektar zlizywały swoimi długimi języczkami siadając na podstawie kwiatu. Cienka łodyga lnu nie stanowi problemu. Pszczoła tak mocno uczepia się łodygi, że nie spada pomimo dyndania jak na trampolinie. Niesamowity owad.</p>
+    <p>Pewnego razu podczas oglądania kwitnącego lnu zauważyłem mnóstwo pszczół. Lądowały na kwiatach i zbierały pyłek. Nektar zlizywały swoimi długimi języczkami siadając na podstawie kwiatu. Cienka łodyga lnu nie stanowiła dla nich problemu. Pszczoła tak mocno uczepia się łodygi, że nie spada pomimo tego, że razem z łodyga podskakuje jak na trampolinie, niesamowity owad.</p>
     
     <Image>
       <Img src={oil_pszczola} width="1080px" />
       <p><i>Pszczoła na kwiecie lnu</i></p>
     </Image>
 
-    <p>Po przekwitnięciu na końcach łodyg wyraźnie widoczne są zielone kulki wypełnione jeszcze świeżymi nasionami. Wraz z upływem czasu i pod wpływem suchej pogody len brązowieje i wysycha. Wyschnięcie powoduje zwiększenie się wolnej pomiędzy nasionami w kulach. Zaczynają wydawać charakterystyczny grzechocący dźwięk po poruszeniu rośliny. 
+    <p>Po przekwitnięciu na czubkach łodyg wyraźnie widoczne są zielone kulki wypełnione jeszcze młodymi nasionami. Wraz z upływem czasu i pod wpływem suchej pogody len brązowieje i wysycha. Wyschnięcie powoduje zwiększenie się wolnej przestrzeni w kulkach, pomiędzy nasionami. Z tego powodu te kulki, po wprawieniu w ruch, zaczynają wydawać charakterystyczny grzechocący dźwięk, podobnie jak grzechotka.</p>
 
     <Image>
       <Img src={oil_len_dojrzaly} width="1080px" />
-      <p><i>Len prawie gotowy do zbiorów</i></p>
+      <p><i>Len prawie gotowy do zbiorów. Na prawej dolnej części zdjęcia został uchwycony fragment uprawy słonecznika.</i></p>
     </Image>
 
-      Taka postać lnu jest często używana przez społeczność Litwinów do tworzenia fantazyjnych wieńców dożynkowych oraz wianków na głowę. Można zobaczyć na filmie video poniżej.</p>
+      <p>Taka postać lnu jest często używana przez społeczność Litwinów do tworzenia fantazyjnych wieńców dożynkowych oraz wianków na głowę. Jeden z takich wieńców można zobaczyć na zdjęciu poniżej.</p>
 
-      {/* <div className="App">
-      <h1>Youtube Embed</h1>
-      <YoutubeEmbed embedId="nNIPgfI4&t" />
-    </div> */}
-<Image>
-<iframe width="1000" height="720"
-src="https://www.youtube.com/embed/fs-nNIPgfI4">
-</iframe> 
-</Image>   
+    <Image>
+      <Img src={oil_wieniec} width="1080px" />
+      <p><i>Ołtarzyk dożynkowy przedstawiający Najświętszy Sakrament. Kielich u podstawy otoczonym jest promieniście odchodzącymi od środka łodygami lnu. Dodatkowo len został wkomponowany jako zwisające frędzle wokół ołtarzyka.</i></p>
+    </Image>  
+
+
+     
+
     {/* [Zdjęcie wyschniętego lnu] [Zdjęcie ozdób z wysuszonego lnu] */}
 
     {/* Zbiory */}
-    <p>W tym roku, przez zeszłoroczne problemy z koszeniem tej rośliny i brak możliwości wynajęcia odpowiednio przygotowanego kombajnu, postanowiłem kupić własny kombajn. Udało mi się kupić stary i używany kombajn, ale miałem czas do dobrego przygotowania go do żniw lnu. Bardzo dobrze naostrzyłem kosę tnącą. Podczas całego koszenia ani razu nie było sytuacji, żeby się zapchała. Jednakże, napotkałem szereg innych problemów, z którymi musiałem sobie poradzić. Jednym z nich było nawijanie się roślin na bęben z nagarniaczami (tu nieoceniona była pomoc mojego kuzyna Matasa). Drugą sprawą było nie wypoziomowane klepisko w młocarni.</p>
+    <p>W tym roku, przez zeszłoroczne doświadczenia z koszeniem tej rośliny i brak możliwości wynajęcia odpowiednio przygotowanego kombajnu, postanowiłem kupić własny kombajn. Udało mi się zakupić 60-letnią, ale ciągle sprawną maszynę. Dzięki temu miałem czas na spokojne przygotowanie kombajnu do żniw. Czas był między innymi na to, żeby bardzo mocno naostrzyć kosę tnącą. Podczas całego koszenia ani razu nie było sytuacji, żeby kosa się zapchała. Jednakże napotkałem szereg innych problemów, z którymi musiałem sobie poradzić. Jednym z nich było nawijanie się roślin na bęben z nagarniaczami (tu nieoceniona była pomoc mojego kuzyna Matasa).
+Drugim problemem było nie wypoziomowane klepisko w młocarni. Poniżej zamieszczam zdjęcie oraz krótki film z koszenia.</p>
     
-    {/* [Zdjęcie z koszenia] */}
+<Image>
+      <Img src={oil_koszenie} width="1080px" />
+      <p><i>Gospodarz z Pomocnikiem na Matadorze. Pod niebem, na którym zachodzące promienie słońca zwiastują nadchodzący zmierzch..</i></p>
+    </Image>  
+
+
+      
+
+
     
-    <p>Ostatecznie udało się zebrać z pola około połowę nasion. Po oczyszczeniu zebranego plonu udało się uzyskać całkiem spory plon.</p>
+  
+
+    <Image>
+      <Img src={oil_nasiona} width="1080px" />
+      <p><i>Siemię lniane po oczyszczeniu, otoczone łodygami lnu zakończonymi grzechocącymi kulkami, z których zostało wymłócone.</i></p>
+    </Image>  
+
+    <Image>
+    <iframe width="315" height="560" 
+src="https://www.youtube.com/embed/2rsyBro9NKo" 
+title="YouTube video player" 
+allow="accelerometer; autoplay; clipboard-write; encrypted-media;
+gyroscope; picture-in-picture;
+web-share;fullscreen"
+></iframe>
+</Image> 
+
+<Xd>
+  <p><i>
+    Krótkie nagranie z koszenia lnu
+    </i></p>
+</Xd>
+
   </Wrapper>
+
+
 );
